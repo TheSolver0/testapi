25abe76873009bd9c48116a69f51972f335795c3
TheSolver0 <lucfotso0@gmail.com>
2024-03-26 19:16:02 +0100

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <title>TestApi</title>
+</head>
+<body>
+    <h1 style="text-align: center;">
+        Listes des articles
+    </h1>
+    <div class="container">
+        <div class="card">
+            <img src="{{asset('images/i.jpg')}}" alt="">
+            <p>
+                description de l'article
+            </p>
+        </div>
+        <div class="card">
+            <img src="{{asset('images/i.jpg')}}" alt="">
+            <p>
+                description de l'article
+            </p>
+        </div> <div class="card">
+            <img src="{{asset('images/i.jpg')}}" alt="">
+            <p>
+                description de l'article
+            </p>
+        </div> <div class="card">
+            <img src="{{asset('images/i.jpg')}}" alt="">
+            <p>
+                description de l'article
+            </p>
+        </div> <div class="card">
+            <img src="{{asset('images/i.jpg')}}" alt="">
+            <p>
+                description de l'article
+            </p>
+        </div> <div class="card">
+            <img src="{{asset('images/i.jpg')}}" alt="">
+            <p>
+                description de l'article
+            </p>
+        </div> <div class="card">
+            <img src="{{asset('images/i.jpg')}}" alt="">
+            <p>
+                description de l'article
+            </p>
+        </div> <div class="card">
+            <img src="{{asset('images/i.jpg')}}" alt="">
+            <p>
+                description de l'article
+            </p>
+        </div> <div class="card">
+            <img src="{{asset('images/i.jpg')}}" alt="">
+            <p>
+                description de l'article
+            </p>
+        </div>
+    </div>
+</body>
+</html>
